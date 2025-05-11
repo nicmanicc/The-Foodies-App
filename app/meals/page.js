4f1c0ba37d92be03b9d2b49b9acec9fe7f59d7f8
Nicholas <nickmanic@hotmail.com>
2025-05-11 22:45:24 +1000

    feat: Create meals grid component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,21 @@
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css';
+import Link from 'next/link';
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          One shared passion: <span className={classes.highlight}>Food</span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/new">Share a Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
