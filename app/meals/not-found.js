7c559bdd8583e33703a6ca995266e114b77954e3
Nicholas <nickmanic@hotmail.com>
2025-05-12 11:32:59 +1000

    feat: Added meal details

---
+++ app/meals/not-found.js
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>
+        The meal you are looking for does not exist. Please check the URL and
+        try again.
+      </p>
+    </main>
+  );
+}
